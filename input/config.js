ea93ce927484907dfb2c73a7bf88bc65cd89f731
blub1337 <blub1337@hotmail.com>
2022-05-08 16:09:54 +0200

    https://github.com/blub1337/nft-landing-page.git

---
+++ input/config.js
@@ -0,0 +1,244 @@
+const fs = require("fs");
+const width = 1000;
+const height = 1000;
+const dir = __dirname;
+const description = "This is an NFT made by L3ET.";
+const baseImageUri = "https://fastidious-clafoutis-d6026a.netlify.app/";
+const startEditionFrom = 1;
+const endEditionAt = 1000;
+const editionSize = 1000;
+const raceWeights = [
+  {
+    value: "skull",
+    from: 1,
+    to: editionSize,
+  },
+];
+
+const races = {
+  skull: {
+    name: "Skull",
+    layers: [
+      {
+        name: "Background",
+        elements: [
+          {
+            id: 0,
+            name: "Light blue",
+            path: `${dir}/1-background/LightBlue.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "Orange",
+            path: `${dir}/1-background/Orange.png`,
+            weight: 20,
+          },
+          {
+            id: 2,
+            name: "Red",
+            path: `${dir}/1-background/Red.png`,
+            weight: 20,
+          },
+          {
+            id: 3,
+            name: "Purple",
+            path: `${dir}/1-background/Purple.png`,
+            weight: 5,
+          },
+          {
+            id: 4,
+            name: "Green",
+            path: `${dir}/1-background/Green.png`,
+            weight: 20,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Suit",
+        elements: [
+          {
+            id: 0,
+            name: "Regular",
+            path: `${dir}/2-suit/Regular.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "Orange",
+            path: `${dir}/2-suit/Orange.png`,
+            weight: 5,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Shoulder",
+        elements: [
+          {
+            id: 0,
+            name: "LunaFlag",
+            path: `${dir}/3-shoulder/LunaFlag.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "USA",
+            path: `${dir}/3-shoulder/USA.png`,
+            weight: 1,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Pin",
+        elements: [
+          {
+            id: 0,
+            name: "Smiley",
+            path: `${dir}/4-pin/Smiley.png`,
+            weight: 1,
+          },
+          {
+            id: 1,
+            name: "LunaBluePin",
+            path: `${dir}/4-pin/LunaBluePin.png`,
+            weight: 100,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Race",
+        elements: [
+          {
+            id: 0,
+            name: "Skull",
+            path: `${dir}/5-skin/Skull.png`,
+            weight: 100,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Facial hair",
+        elements: [
+          {
+            id: 0,
+            name: "No facial hair",
+            path: `${dir}/6-facial-hair/NoFacialHair.png`,
+            weight: 100,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Mask",
+        elements: [
+          {
+            id: 0,
+            name: "No mask",
+            path: `${dir}/7-mask/NoMask.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "Medical",
+            path: `${dir}/7-mask/mask.png`,
+            weight: 1,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Hair",
+        elements: [
+          {
+            id: 0,
+            name: "Blonde bun",
+            path: `${dir}/8-hair/BlondeBun.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "Pink",
+            path: `${dir}/8-hair/Pink.png`,
+            weight: 5,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Accessories",
+        elements: [
+          {
+            id: 0,
+            name: "No accessories",
+            path: `${dir}/9-accessories/NoAcc.png`,
+            weight: 100,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+      {
+        name: "Headwear",
+        elements: [
+          {
+            id: 0,
+            name: "Glass dome",
+            path: `${dir}/10-headwear/GlassDome.png`,
+            weight: 100,
+          },
+          {
+            id: 1,
+            name: "Helmet",
+            path: `${dir}/10-headwear/Helmet.png`,
+            weight: 20,
+          },
+          {
+            id: 2,
+            name: "Headset",
+            path: `${dir}/10-headwear/Headset.png`,
+            weight: 20,
+          },
+          {
+            id: 3,
+            name: "Robber",
+            path: `${dir}/10-headwear/Robber.png`,
+            weight: 20,
+          },
+          {
+            id: 4,
+            name: "NFTHelmet",
+            path: `${dir}/10-headwear/NFTHelmet.png`,
+            weight: 5,
+          },
+        ],
+        position: { x: 0, y: 0 },
+        size: { width: width, height: height },
+      },
+    ],
+  },
+};
+
+module.exports = {
+  width,
+  height,
+  description,
+  baseImageUri,
+  editionSize,
+  startEditionFrom,
+  endEditionAt,
+  races,
+  raceWeights,
+};
